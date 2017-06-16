c642603f9077619fe3d0ac24ee83bbb334ca6f13
sidney zhang <sidneyzsc@gmail.com>
2017-06-16 18:08:26 +0800

    20170616 commit

--- src/main/java/com/testmvc/controller/AccountController.java
+++ src/main/java/com/testmvc/controller/AccountController.java
@@ -6,15 +6,13 @@ import org.apache.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.ui.ModelMap;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.ResponseBody;
 import org.thymeleaf.util.ListUtils;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 /**
  * AccountController
@@ -38,18 +36,18 @@ public class AccountController {
     @RequestMapping("/accountinfo")
     public String showAccount(Model model,final Account account) {
         log.info("查询账户信息");
-//        List<Account> accountinfo= this.accountService.getAccountList();
-//        log.info("账户条数:"+ ListUtils.size(accountinfo));
+        List<Account> accountinfo= this.accountService.getAccountList();
+        log.info("账户条数:"+ ListUtils.size(accountinfo));
         this.prepareAccountInfo();
         return "accountinfo";
 
     }
 
     @RequestMapping(value = "/accountinfo",params = {"save"})
-    public String saveAccount(@ModelAttribute(value = "account") Account account, final BindingResult bindingResult, final ModelMap model){
+    public String saveAccount(@ModelAttribute(value = "account") Account account, final BindingResult bindingResult, final Model model){
         log.info("新增账户信息");
         accountService.insertAccount(account);
-        model.clear();
+        model.asMap().clear();
         return "redirect:/accountinfo";
     }
 
